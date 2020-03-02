1d6c072db9393a646b7bc384b1911bf76ddfb720
Mivehnejad <Kayvan.Mivehnejad@vce.com>
2020-03-02 14:06:12 -0500

    "array of inputs instead of firstvsanitem"

--- src/app/vsan.component.ts
+++ src/app/vsan.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input ,Output , EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'vsan-form2',
@@ -7,5 +7,11 @@ import { Component, Input } from '@angular/core'
 })
 
 export class VsanComponent{ 
-    @Input() vsanItem; 
+    @Input() vsanItem;
+    @Output() delete = new EventEmitter();
+
+    onDelete(){
+        console.log('deleted');
+        this.delete.emit(this.vsanItem);
+    }
 }
--- src/app/vsanform.component.ts
+++ src/app/vsanform.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core'
+import { Component, Input, Output} from '@angular/core'
 
 @Component({
     selector: 'vsan-form1',
@@ -11,13 +11,43 @@ export class VsanformComponent{
     
    /* onSubmit(){
         console.log('test');*/
-        firstvsanItem={
+        /*firstvsanItem={
             id:1,
             numNodes:'12' ,
             numCapdisks:'4' ,
             numDiskgroups:'2' ,
             ssdSize:'800' ,
             FTM:'PFTT=1'
-        };
+        };*/
+        vsanItems=[
+            {
+                id:1,
+                numNodes:'12' ,
+                numCapdisks:'4' ,
+                numDiskgroups:'2' ,
+                ssdSize:'800' ,
+                FTM:'PFTT=1'
+            },
+            {
+                id:2,
+                numNodes:'8' ,
+                numCapdisks:'4' ,
+                numDiskgroups:'2' ,
+                ssdSize:'1200' ,
+                FTM:'PFTT=2'
+            },
+            {
+                id:3,
+                numNodes:'12' ,
+                numCapdisks:'4' ,
+                numDiskgroups:'2' ,
+                ssdSize:'2500' ,
+                FTM:'PFTT=3'
+            }
+
+        ]
+        onVsanItemDelete(vsanItem){
+
+        }
     }
 
