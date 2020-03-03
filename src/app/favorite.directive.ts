23b8446f0883acd7ea58090d5085592c111ae88e
mivehk <kayvan.mivehnejad@dell.com>
2020-03-03 12:50:19 -0500

    fav directive listening to mouse events

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,11 +1,23 @@
-import {Directive ,HostBinding, Input} from '@angular/core';
+import {Directive ,HostBinding, Input, HostListener} from '@angular/core';
 
 @Directive({
     selector: '[cuFavorite]'
 })
-
+//directive change host element properties via host binding
+//directive can respond to host event changes via host listeners
 export class FavoriteDirective {
-    @HostBinding('class.is-favorite') isFavorite =true;
+
+    /*inside parenteses we select css class name which will be added to element once value of 
+    bolean isFavorite is true which also as a css selector it changes color of  */
+    @HostBinding('class.is-fav') isFavorite =true;
+    @HostBinding('class.is-fav-hovering') hovering = false;
+   //for events mouseenter and mouseleave we define reponses which toggle hoveringvalue
+    @HostListener('mouseenter') onMouseEnter(){
+        this.hovering=true
+    }
+    @HostListener('mouseleave') onMouseLeave(){
+        this.hovering= false
+    }
     @Input() set cuFavorite(value: boolean){
         this.isFavorite = value;
     }
