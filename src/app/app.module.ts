64792a0a3db64c468d7f199bca2dc358a3635a0f
mivehk <kayvan.mivehnejad@dell.com>
2020-03-04 11:22:27 -0500

    Added input validators into cuname and ssdsize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import {AppComponent} from './app.component'
 import {BrowserModule} from '@angular/platform-browser'
-import {FormsModule} from '@angular/forms'
+import {ReactiveFormsModule} from '@angular/forms'
 import {VsanformComponent} from './vsanform.component'
 import {VsanComponent} from './vsan.component'
 import {FavoriteDirective} from './favorite.directive'
@@ -11,7 +11,7 @@ import {VsanListComponent} from './vsanlist.component'
 @NgModule({
     imports:[
         BrowserModule,
-        FormsModule
+        ReactiveFormsModule
     ],
     declarations:[
         AppComponent,
--- src/app/vsanform.component.ts
+++ src/app/vsanform.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
 
 @Component({
     selector: 'vsan-form-1',
@@ -8,6 +10,33 @@ import { Component } from '@angular/core'
 })
 
 export class VsanformComponent{
+     form: FormGroup;
+     ngOnInit(){
+         this.form =new FormGroup({
+             cuName: new FormControl('',Validators.compose([
+                 Validators.required
+             ])),
+             numNodes: new FormControl(),
+             numCapdisks:new FormControl(),
+             numDiskgroups: new FormControl(),
+             ssdSize: new FormControl('',this.ssdSizeValidator),
+             FTM:new FormControl(),
+             isFavorite: new FormControl()
+         })
+     }   
+     ssdSizeValidator(control: FormControl){
+         
+         const ssdSize= parseInt(control.value , 10)
+    
+         const minSSD = 500;
+         const maxSSD = 62000;
+         if ( ssdSize >= minSSD && ssdSize <= maxSSD){
+             return null
+         } else{
+             return { ssdSize: true}
+         }
+
+     } 
      onSubmit(vsanItem){
              console.log(vsanItem);
      }         
