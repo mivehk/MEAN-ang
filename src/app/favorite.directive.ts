caf422a4dd41161794b22701239057378f7794c8
mivehk <kayvan.mivehnejad@dell.com>
2020-03-05 10:54:42 -0500

    title changes plus some comments

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -8,7 +8,7 @@ import {Directive ,HostBinding, Input, HostListener} from '@angular/core';
 export class FavoriteDirective {
 
     /*inside parenteses we select css class name which will be added to element once value of 
-    bolean isFavorite is true which also as a css selector it changes color of  */
+    bolean isFavorite is true which also as a css selector it changes color of heart svg */
     @HostBinding('class.is-fav') isFavorite =true;
     @HostBinding('class.is-fav-hovering') hovering = false;
    //for events mouseenter and mouseleave we define reponses which toggle hoveringvalue
